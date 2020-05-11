e334044e70ca7c54b04f9c7feb7cc33c021a0d8d
Steven George <steven.george@hotmail.co.uk>
2020-05-11 13:05:01 +0100

    finished imputation of null categorical in application dataset

--- src/data/preprocessing.py
+++ src/data/preprocessing.py
@@ -12,24 +12,30 @@ CATEGORICAL_FEATURES_WITH_NULLS = [
 Categorical features in training set which had nulls
 """
 
-NAME_TYPE_SUITE_VALUE_MAP = {
-    'Other_A': 'Other',
-    'Other_B': 'Other',
-    np.nan: 'Other'
-}
-
 
 def impute_null_categorical_features(X):
     X = X.copy()
 
     # NAME_TYPE_SUITE
-    X['NAME_TYPE_SUITE'].replace(NAME_TYPE_SUITE_VALUE_MAP, inplace=True)
+    name_type_suite_value_map = {
+        'Other_A': 'Other',
+        'Other_B': 'Other',
+        np.nan: 'Other'
+    }
+
+    X['NAME_TYPE_SUITE'].replace(name_type_suite_value_map, inplace=True)
 
     # OCCUPATION_TYPE
     X.loc[
         (X['NAME_INCOME_TYPE'] == 'Unemployed'),
         'OCCUPATION_TYPE'
     ] = 'Unemployed'
+
+
+    X.loc[
+        (X['NAME_INCOME_TYPE'] == 'Pensioner') & (X['OCCUPATION_TYPE'].isnull()),
+        'OCCUPATION_TYPE'
+    ] = 'Pensioner'
 
     for name_income_type in ['Commercial associate', 'State servant', 'Working', 'Student']:
 
@@ -38,11 +44,23 @@ def impute_null_categorical_features(X):
             'OCCUPATION_TYPE'
         ] = 'Laborers'
 
-    X.loc[
-        (X['NAME_INCOME_TYPE'] == 'Pensioner') & (X['OCCUPATION_TYPE'].isnull()),
-        'OCCUPATION_TYPE'
-    ] = 'Pensioner'
-
     # FONDKAPREMONT_MODE
     X.drop('FONDKAPREMONT_MODE', axis=1, inplace=True)
 
+    # HOUSETYPE_MODE
+    X.loc[X['HOUSETYPE_MODE'] != 'block of flats', 'HOUSETYPE_MODE'] = 'not block of flats'
+
+    # WALLSMATERIAL_MODE
+    X['WALLSMATERIAL_MODE'].fillna('Unknown', inplace=True)
+
+    # EMERGENCYSTATE_MODE
+    X['EMERGENCYSTATE_MODE'].fillna('Unknown', inplace=True)
+
+    object_columns = X.select_dtypes(include=object).columns
+    categorical_features_with_nulls = list(X[object_columns].isnull().sum()[X[object_columns].isnull().sum() > 0].index)
+    new_categorical_features_with_nulls = set(categorical_features_with_nulls) - set(CATEGORICAL_FEATURES_WITH_NULLS)
+
+    assert len(new_categorical_features_with_nulls) == 0, \
+        f"New categorical features with nulls {new_categorical_features_with_nulls}"
+
+    return X
